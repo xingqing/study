924d7ec4c1ff416877097380fb2623d34e6b9421
xingqing <xingvqing@gmail.com>
2019-04-26 13:56:58 +0800

    代码提交

--- src/test/java/com/codefuntest/Test.java
+++ src/test/java/com/codefuntest/Test.java
@@ -1,13 +1,18 @@
 package com.codefuntest;
-/** 
- * 
- * @author 作者 :		E-mail: 
- * @version 创建时间：2016-7-12 下午2:27:50 
- * 
- */
+
+
+import org.ho.yaml.Yaml;
+
+import java.io.InputStream;
+import java.util.HashMap;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		try(InputStream inputStream = Test.class.getClassLoader().getResourceAsStream("test.yml")){
+			HashMap<String,Object> map = Yaml.loadType(inputStream, HashMap.class);
+			System.out.println(map);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 }
